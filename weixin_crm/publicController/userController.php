d6aac1845f05f72fb5e901ec8014d75a5034aef3
ShiinaMinatus <xij1111@gmail.com>
2014-03-10 17:52:48 +0800

    修正冲突提交
    Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

--- weixin_crm/publicController/userController.php
+++ weixin_crm/publicController/userController.php
@@ -2,129 +2,111 @@
 
 class userController implements User {
 
-	// 用户列表 界面
-	public function userList(){
+    // 用户列表 界面
+    public function userList() {
+        $userModel->initialize();
+        $result = $userModel->vars_all;
+        var_dump($result);
+        $_ENV['smarty']->setDirTemplates('user');
+        $_ENV['smarty']->assign('userInfo', $result);
+        $_ENV['smarty']->display('userList');
+    }
 
-	
-	}
+    public function addUser($data) {
 
-
-<<<<<<< HEAD
-		 $userModel->initialize();
-                $result =$userModel->vars_all;
-                var_dump($result);
-		$_ENV['smarty']->setDirTemplates('user');
-		$_ENV['smarty']->assign('userInfo',$result);
-		$_ENV['smarty']->display('userList');
-                
-=======
-	public function addUser($data){
-
-		if(is_array($data) && count($data) > 0){
->>>>>>> 19d93d6c6eac8738a061c5368c221be3ef1afed1
-
-			$userModel = new userModel();
-
-		    $userModel->insert($data);
-		}
-	}
-	
-
-	public function updateUser($data,$user_id){
-
-		if(is_array($data) && count($data) > 0){
-
-			$userModel = new userModel();
-
-			$userModel->initialize('user_id = '.$user_id);
-
-			if($userModel->vars_number >0){
-
-				 $userModel->update($data);
-			}
-		}
-
-	}
+        if (is_array($data) && count($data) > 0) {
 
 
-	public function deleteUser($user_id){
+            $userModel = new userModel();
 
+            $userModel->insert($data);
+        }
+    }
 
-	}
+    public function updateUser($data, $user_id) {
 
-	public function searchUser(){
+        if (is_array($data) && count($data) > 0) {
 
+            $userModel = new userModel();
 
-	}
+            $userModel->initialize('user_id = ' . $user_id);
 
+            if ($userModel->vars_number > 0) {
+
+                $userModel->update($data);
+            }
+        }
+    }
+
+    public function deleteUser($user_id) {
+        
+    }
+
+    public function searchUser() {
+        
+    }
 
     /**
-	 * 增加用户积分
-	 *
-	 * user_id  int  用户id
-	 *
-	 * pointer  int   需要增加的积分
-	 */
+     * 增加用户积分
+     *
+     * user_id  int  用户id
+     *
+     * pointer  int   需要增加的积分
+     */
+    public function addPointer($user_id, $integration) {
 
-	public function addPointer($user_id,$integration){
+        $user = new userModel();
 
-		$user = new userModel();
+        $user->initialize('user_id = ' . $user_id);
 
-		$user->initialize('user_id = '.$user_id);
+        if ($user->vars_number > 0) {
 
-		if($user->vars_number >0){
+            $user->vars['user_integration']+=$pointer;
 
-			$user->vars['user_integration']+=$pointer;
+            $user_pointer_record = new userPointerRecordModel();
 
-			$user_pointer_record = new userPointerRecordModel();
+            $record['user_id'] = $user_id;
 
-			$record['user_id'] = $user_id;
+            $record['record_type'] = 1;
 
-			$record['record_type'] = 1;
+            $record['fraction'] = $integration;
 
-			$record['fraction'] = $integration;
+            $record['source'] = 'crm';
 
-			$record['source'] = 'crm';
+            $record['create_time'] = time();
 
-			$record['create_time'] = time();
+            $user_pointer_record->insert($record);
+        }
+    }
 
-			$user_pointer_record->insert($record);
-
-		}
-
-
-	}
-
-	public function addMoney($user_id,$money){
+    public function addMoney($user_id, $money) {
 
 
-		$user = new userModel();
+        $user = new userModel();
 
-		$user->initialize('user_id = '.$user_id);
+        $user->initialize('user_id = ' . $user_id);
 
-		if($user->vars_number >0){
+        if ($user->vars_number > 0) {
 
-			$user->vars['user_money']+=$money;
+            $user->vars['user_money']+=$money;
 
-			$user->updateVars();
+            $user->updateVars();
 
-			$user_pointer_record = new userPointerRecordModel();
+            $user_pointer_record = new userPointerRecordModel();
 
-			$record['user_id'] = $user_id;
+            $record['user_id'] = $user_id;
 
-			$record['record_type'] = 2;
+            $record['record_type'] = 2;
 
-			$record['fraction'] = $integration;
+            $record['fraction'] = $integration;
 
-			$record['source'] = 'crm';
+            $record['source'] = 'crm';
 
-			$record['create_time'] = time();
+            $record['create_time'] = time();
 
-			$user_pointer_record->insert($record);
-
-		}
-	}
-
+            $user_pointer_record->insert($record);
+        }
+    }
 
 }
 
