39f7917caa8910609c87da5eba681f11c374f725
jinglingchang <jinglingchang@126.com>
2014-03-10 17:21:28 +0800

    用户接口

--- weixin_crm/publicController/userController.php
+++ weixin_crm/publicController/userController.php
@@ -3,41 +3,51 @@
 class userController implements User {
 
 	// 用户列表 界面
-
 	public function userList(){
 
 		$userModel = new userModel();
 
 		$result = $userModel->initialize();
 
+
+		$_ENV['smarty']->assign('name',1234);
+
 		$_ENV['smarty']->setDirTemplates('user');
-<<<<<<< HEAD
-
-		$_ENV['smarty']->assign('name','1234');
 
 		$_ENV['smarty']->display('userList');
 
-=======
-		$_ENV['smarty']->assign('name','1234');
-		$_ENV['smarty']->display('userList');
->>>>>>> 5066812e6b07419f9416fed27ce6e28187cb2f28
 	}
 
 
-	public function addUser(){
+	public function addUser($data){
 
-		
+		if(is_array($data) && count($data) > 0){
 
+			$userModel = new userModel();
+
+		    $userModel->insert($data);
+		}
 	}
 	
 
-	public function updateUser(){
+	public function updateUser($data,$user_id){
 
+		if(is_array($data) && count($data) > 0){
+
+			$userModel = new userModel();
+
+			$userModel->initialize('user_id = '.$user_id);
+
+			if($userModel->vars_number >0){
+
+				 $userModel->update($data);
+			}
+		}
 
 	}
 
 
-	public function deleteUser(){
+	public function deleteUser($user_id){
 
 
 	}
@@ -46,6 +56,75 @@ class userController implements User {
 
 
 	}
+
+
+    /**
+	 * 增加用户积分
+	 *
+	 * user_id  int  用户id
+	 *
+	 * pointer  int   需要增加的积分
+	 */
+
+	public function addPointer($user_id,$integration){
+
+		$user = new userModel();
+
+		$user->initialize('user_id = '.$user_id);
+
+		if($user->vars_number >0){
+
+			$user->vars['user_integration']+=$pointer;
+
+			$user_pointer_record = new userPointerRecordModel();
+
+			$record['user_id'] = $user_id;
+
+			$record['record_type'] = 1;
+
+			$record['fraction'] = $integration;
+
+			$record['source'] = 'crm';
+
+			$record['create_time'] = time();
+
+			$user_pointer_record->insert($record);
+
+		}
+
+
+	}
+
+	public function addMoney($user_id,$money){
+
+
+		$user = new userModel();
+
+		$user->initialize('user_id = '.$user_id);
+
+		if($user->vars_number >0){
+
+			$user->vars['user_money']+=$money;
+
+			$user->updateVars();
+
+			$user_pointer_record = new userPointerRecordModel();
+
+			$record['user_id'] = $user_id;
+
+			$record['record_type'] = 2;
+
+			$record['fraction'] = $integration;
+
+			$record['source'] = 'crm';
+
+			$record['create_time'] = time();
+
+			$user_pointer_record->insert($record);
+
+		}
+	}
+
 
 }
 
