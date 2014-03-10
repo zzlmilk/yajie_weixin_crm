d7b23eb52e972a9adf2386c821d953b2e8a7bea6
jinglingchang <jinglingchang@126.com>
2014-03-10 17:25:15 +0800

    用户接口

--- weixin_crm/config/database.php
+++ weixin_crm/config/database.php
@@ -2,9 +2,9 @@
 
 $database = array(
 
-	'admin'=>array('DBNAME'=>'weixin','DBHOST'=>'localhost','USER'=>'root','PASSWORD'=>'123456'),
+	'admin'=>array('DBNAME'=>'weixin','DBHOST'=>'localhost','USER'=>'root','PASSWORD'=>''),
 
-	'company'=>array('DBNAME'=>'weixin_company','DBHOST'=>'localhost','USER'=>'root','PASSWORD'=>'123456')
+	'company'=>array('DBNAME'=>'weixin_company','DBHOST'=>'localhost','USER'=>'root','PASSWORD'=>'')
 
 );
 
