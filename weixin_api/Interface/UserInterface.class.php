cfeb0975724aa6e08bcdde570c57a4ffd417446f
jinglingchang <jinglingchang@126.com>
2014-03-11 16:58:57 +0800

    用户api

--- weixin_api/Interface/UserInterface.class.php
+++ weixin_api/Interface/UserInterface.class.php
@@ -6,21 +6,7 @@ interface user {
 	 * 用户注册  v1
 	 */
 	public function add();
-	/**
-	 * 获取单个用户信息  v1
-	 */
 
-	public function get_info();
-	/**
-	 *  获取全部用户信息  v1
-	 */
-
-	public function get_info_all();
-	/**
-	 * 修改用户信息  v1
-	 */
-
-	public function  revise_user();
 	/**
 	 * 用户积分 累加  v1
 	 */
