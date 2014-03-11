4063261925971cf2b03e82ff13d5221ccc10e6ac
ShiinaMinatus <xij1111@gmail.com>
2014-03-11 14:06:19 +0800

    消费
    Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

--- weixin_crm/publicController/userController.php
+++ weixin_crm/publicController/userController.php
@@ -12,6 +12,7 @@ class userController implements User {
         $_ENV['smarty']->display('userList');
     }
 
+    //编辑用户信息
     public function userEdit() {
         $errorFlag = true;
         if (isset($_GET['userId'])) {
@@ -21,6 +22,19 @@ class userController implements User {
             $userCount = $userModel->vars_number;
             if ($userCount > 0) {
                 $userData = $userModel->vars;
+                $userPointerRecord = new userPointerRecordModel();
+                $userPointerRecord->initialize("user_id='$userId' and record_type=1");
+                if ($userPointerRecord->vars_number > 0) {
+                    $userPointData = $userPointerRecord->vars_all;
+                } else {
+                    $userPointData = 0;
+                }
+                $userPointerRecord->initialize("user_id='$userId' and record_type=2");
+                if ($userPointerRecord->vars_number > 0) {
+                    $userMoneyData = $userPointerRecord->vars_all;
+                } else {
+                    $userMoneyData = 0;
+                }
             } else {
                 $errorFlag = FALSE;
                 $errorMessage = "未搜索到该用户请刷新后重试";
@@ -32,21 +46,24 @@ class userController implements User {
         $_ENV['smarty']->setDirTemplates('user');
         $returnData = $errorFlag ? $userData : $errorMessage;
         $_ENV['smarty']->assign('userData', $returnData);
+        $_ENV['smarty']->assign('userPointData', $userPointData);
+        $_ENV['smarty']->assign('userMoneyData', $userMoneyData);
         $_ENV['smarty']->assign('errorFlag', $errorFlag);
         $_ENV['smarty']->display('userEdit');
     }
 
+//提交更新的用户信息
     public function userUpdata() {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (isset($_POST)) {
-                $userId=$_POST['user_id'];
-                $upDatas['user_name']       =   $_POST['user_name'];
-                $upDatas['user_phone']      =   $_POST['user_phone'];
-                $upDatas['birthday']        =   $_POST['birthday'];
-                $upDatas['sex']             =   $_POST['sex'];
-                $upDatas['user_money']      =   $_POST['user_money'];
-                $upDatas['user_integration'] =  $_POST['user_integration'];
-                $this->updateUser($upDatas,$userId);
+                $userId = $_POST['user_id'];
+                $upDatas['user_name'] = $_POST['user_name'];
+                $upDatas['user_phone'] = $_POST['user_phone'];
+                $upDatas['birthday'] = $_POST['birthday'];
+                $upDatas['sex'] = $_POST['sex'];
+                $upDatas['user_money'] = $_POST['user_money'];
+                $upDatas['user_integration'] = $_POST['user_integration'];
+                $this->updateUser($upDatas, $userId);
                 $this->userList();
             } else {
                 
@@ -56,14 +73,56 @@ class userController implements User {
         }
     }
 
+    //积分信息界面
+    public function pointManage() {
+        $userModel = new userModel();
+        $userModel->initialize();
+        $result = $userModel->vars_all;
+        $_ENV['smarty']->setDirTemplates('user');
+        $_ENV['smarty']->assign('userInfo', $result);
+        $_ENV['smarty']->display('pointManage');
+    }
+
+    public function moneyManage() {
+        $userModel = new userModel();
+        $userModel->initialize();
+        $result = $userModel->vars_all;
+        $_ENV['smarty']->setDirTemplates('user');
+        $_ENV['smarty']->assign('userInfo', $result);
+        $_ENV['smarty']->display('moneyManage');
+    }
+
+    public function moneyConturl() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($_POST["moneyNumber"] >= 0) {
+                $userId = $_POST['userId'];
+                $moneyNumber = $_POST['moneyNumber'];
+                switch ($_POST["moneyType"]) {
+                    case "deductMoney":
+                        $moneyConturlRequest = $this->addMoney($userId, $moneyNumber * (-1));
+                        break;
+                    case "recharge":
+                        $moneyConturlRequest = $this->addMoney($userId, $moneyNumber);
+                        break;
+                }
+                if ($moneyConturlRequest > 0) {
+                    echo "操作成功！";
+                    $this->moneyManage();
+                }
+            } else {
+                
+            }
+        }
+    }
+
     public function addUser($data) {
 
         if (is_array($data) && count($data) > 0) {
 
             $userModel = new userModel();
 
-          $returnVal=  $userModel->insert($data);
-          return $returnVal;
+            $returnVal = $userModel->insert($data);
+            return $returnVal;
         }
     }
 
@@ -142,7 +201,7 @@ class userController implements User {
 
             $record['record_type'] = 2;
 
-            $record['fraction'] = $integration;
+            $record['fraction'] = $money;
 
             $record['source'] = 'crm';
 
--- weixin_crm/templates/user/userEdit.tpl
+++ weixin_crm/templates/user/userEdit.tpl
@@ -1,25 +1,34 @@
 {if $errorFlag eq 1}
     <form action="{$WebSiteUrl}/pageredirst.php?action=user&functionname=userUpdata" method="post">
-    客户姓名：<input type="text" value="{$userData.user_name}" name="user_name" id="userName"><br>
-    客户电话：<input type="text" value="{$userData.user_phone}" name="user_phone" id="userPhone"><br>
-    客户生日：<input type="text" value="{$userData.birthday}" name="birthday" id="userBirthday"><br>
-    客户性别：<select name="sex" id='sex'>
-        {if $userData.sex eq '1'}
-            <option value="1" selected="selected">男</option>
-            <option value="2">女</option>
-        {else}
-            <option value="1">男</option>
-            <option value="2" selected="selected">女</option>
-        {/if}
-    </select><br>
-    客户余额：<input type="text" value="{$userData.user_money}" name="user_money" id="userMoney"><br>
-    客户积分：<input type="text" value="{$userData.user_integration}" name="user_integration" id="userIntegration"><br>
-    <input type="hidden" value="{$userData.user_id}" name="user_id" id="user_id">
-    <button onclick="submit();">保存修改</button></a>
-    </form>
+        客户姓名：<input type="text" value="{$userData.user_name}" name="user_name" id="userName"><br>
+        客户电话：<input type="text" value="{$userData.user_phone}" name="user_phone" id="userPhone"><br>
+        客户生日：<input type="text" value="{$userData.birthday}" name="birthday" id="userBirthday"><br>
+        客户性别：<select name="sex" id='sex'>
+            {if $userData.sex eq '1'}
+                <option value="1" selected="selected">男</option>
+                <option value="2">女</option>
+            {else}
+                <option value="1">男</option>
+                <option value="2" selected="selected">女</option>
+            {/if}
+        </select><br>
+        客户余额：<input type="text" value="{$userData.user_money}" name="user_money" readonly="readonly" id="userMoney"><br>
+        客户积分：<input type="text" value="{$userData.user_integration}" name="user_integration" readonly="readonly" id="userIntegration"><br>
+        <input type="hidden" value="{$userData.user_id}" name="user_id" id="user_id">
+        <button onclick="submit();">保存修改</button></a>
+</form>
+
 {else}
     {$userData} <!-- 此处返回错误信息-->
-
+{/if}
+{if $userMoneyData eq 0}
+    暂无消费数据
+{else}
+{/if}
+<br>
+{if $userPointData eq 0}
+    暂无积分数据
+{else}
 {/if}
 
 
