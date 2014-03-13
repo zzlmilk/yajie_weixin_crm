4d7bb7d01871be09bc115f8ae433160af87140e6
Your Name Here <your_email@example.com>
2014-03-13 12:17:51 +0800

    用户激活

--- weixin_crm/config/database.php
+++ weixin_crm/config/database.php
@@ -2,9 +2,9 @@
 
 $database = array(
 
-	'admin'=>array('DBNAME'=>'weixin','DBHOST'=>'localhost','USER'=>'root','PASSWORD'=>'123456'),
+	'admin'=>array('DBNAME'=>'weixin','DBHOST'=>'127.0.0.1','USER'=>'root','PASSWORD'=>'123456'),
 
-	'company'=>array('DBNAME'=>'weixin_company','DBHOST'=>'localhost','USER'=>'root','PASSWORD'=>'123456')
+	'company'=>array('DBNAME'=>'weixin_company','DBHOST'=>'127.0.0.1','USER'=>'root','PASSWORD'=>'123456')
 );
 
 $_ENV['database'] = $database;
