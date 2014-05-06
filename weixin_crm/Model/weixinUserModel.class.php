48430c7ae1f8c292f37a3fbfdb33a23f66323f9c
ShiinaMinatus <xij1111@gmail.com>
2014-05-06 15:23:16 +0800

    Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

---
+++ weixin_crm/Model/weixinUserModel.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of weixinUserModel
+ *
+ * @author Administrator
+ */
+class weixinUserModel extends Basic{
+        public function __construct() {
+
+        $this->child_name = 'weixin_user';
+
+        parent::__construct();
+    }
+}
+
+?>
