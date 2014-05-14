1ba954be00f8343457352f1e9efd712163095a63
ShiinaMinatus <xij1111@gmail.com>
2014-05-14 12:28:46 +0800

    Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

--- weixin_crm/config/database.php
+++ weixin_crm/config/database.php
@@ -1,12 +1,9 @@
 <?php
 
 $database = array(
-	'admin'=>array('DBNAME'=>'weixin','DBHOST'=>'127.0.0.1','USER'=>'root','PASSWORD'=>''),
-
-	'company'=>array('DBNAME'=>'weixin_company','DBHOST'=>'127.0.0.1','USER'=>'root','PASSWORD'=>''),
-
-       'Inhouse' => array('DBNAME' => 'weixin_inhouse', 'DBHOST' => '127.0.0.1', 'USER' => 'root', 'PASSWORD' => '')
-
+    'admin' => array('DBNAME' => 'weixin', 'DBHOST' => '127.0.0.1', 'USER' => 'root', 'PASSWORD' => '123456'),
+    'company' => array('DBNAME' => 'weixin_company', 'DBHOST' => '127.0.0.1', 'USER' => 'root', 'PASSWORD' => '123456'),
+    'Inhouse' => array('DBNAME' => 'weixin_inhouse', 'DBHOST' => '127.0.0.1', 'USER' => 'root', 'PASSWORD' => '123456')
 );
 
 $_ENV['database'] = $database;
