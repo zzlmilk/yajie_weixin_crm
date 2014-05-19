f5b34b80a04683c3a1ad69ded93fdaee042bc87f
ShiinaMinatus <xij1111@gmail.com>
2014-05-19 12:20:58 +0800

    修正消费记录样式 Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

---
+++ weixin_crm/Model/exchangeCodeVerificationModel.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of exchangeCodeVerification
+ *
+ * @author Administrator
+ */
+class exchangeCodeVerificationModel extends Basic {
+
+    public function __construct() {
+
+        $this->child_name = 'exchange_code_verification';
+
+        parent::__construct();
+    }
+
+}
+
+?>
